193df6152af20d4306c90727739f90709910082a
smacander <smacander@bitbucket.org>
2022-02-21 14:56:01 -0600

    Played around with the views and URL to create a /uploads page

--- iron_handmaidens/data/urls.py
+++ iron_handmaidens/data/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='data-home')
+    path('', views.home, name='data-home'),
+    path('upload', views.upload)
 ]
--- iron_handmaidens/data/views.py
+++ iron_handmaidens/data/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'data/home.html')
+
+def upload(request):
+    return render(request, 'data/upload.html')
