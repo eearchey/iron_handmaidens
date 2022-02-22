46330facb60ea9ded03694c8cda9b1cde68bad7f
emilyarchey <eearchey@crimson.ua.edu>
2022-02-22 14:43:57 -0600

    Ability to access csv file in backend

--- iron_handmaidens/data/urls.py
+++ iron_handmaidens/data/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='data-home')
+    path('', views.home, name='data-home'),
+    path('visualize/', views.visualize, name='data-visualize')
 ]
--- iron_handmaidens/data/views.py
+++ iron_handmaidens/data/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+import csv
+import io
 
 def home(request):
+    if request.method == 'POST' and request.FILES['csv-file']:
+        csv_file = request.FILES['csv-file']
+        filename = csv_file.name
+        # Read csv file InMemoryUploadedFile
+        file = csv_file.read().decode('utf-8-sig')
+        return render(request, 'data/visualize.html', {'filename': filename})
     return render(request, 'data/home.html')
+
+def visualize(request):
+    return render(request, 'data/visualize.html')
+
+def handle_uploaded_file(f):
+    with open('some/file/name.txt', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
