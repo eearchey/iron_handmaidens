c2c8c35824f3aae082e63184870edb069d3928b1
Alden Moreton <aldenmoreton99@gmail.com>
2022-04-25 08:42:29 -0500

    Changed from dev to production in setting

--- iron_handmaidens/iron_handmaidens/settings.py
+++ iron_handmaidens/iron_handmaidens/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mm)^nmflpc%3c$0yblrx^lv#jti471y9)0*sd^2vn@=0b_!(!e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
